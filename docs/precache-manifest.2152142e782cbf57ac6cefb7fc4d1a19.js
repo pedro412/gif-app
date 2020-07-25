094842edac0dffbdc033c1a119087e0cfb5a79c3
Pedro Alvarez <pedro.alvarez412@outlook.com>
2020-07-25 16:37:43 -0500

    [ADD] Docs

---
+++ docs/precache-manifest.2152142e782cbf57ac6cefb7fc4d1a19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e117c53641493430244f314b9433c3e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b8447a5f281cb156d3b",
+    "url": "/static/css/main.07a410ec.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b8447a5f281cb156d3b",
+    "url": "/static/js/main.4601a6bc.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
