5772a36349717f9918b083a94f6a476224c1de63
pedro412 <pedro.alvarez412@outlook.com>
2020-08-04 22:06:01 -0500

    added gifGridItem tests

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import '@testing-library/jest-dom';
 const { shallow } = require('enzyme');
 const { default: GifGridItem } = require('../../components/GifGridItem');
 
-describe('Testing <GifGridItem />', () => {
-  test('It should render correctly', () => {
-    const title = 'Hola';
-    const url = 'https://algo.com';
-    const wrapper = shallow(<GifGridItem title={title} url={url} />);
+describe('testing <GifGridItem />', () => {
+  const title = 'Hola';
+  const url = 'https://algo.com';
+  const wrapper = shallow(<GifGridItem title={title} url={url} />);
 
+  test('it should render correctly', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  test('it should containt a paragraph with the title', () => {
+    const p = wrapper.find('p');
+    expect(p.text()).toBe(title);
+  });
+
+  test('it should contain an img tag with the src equal as the img url passed', () => {
+    const img = wrapper.find('img');
+    expect(img.prop('src')).toBe(url);
+  });
+
+  test('it should contain the class grid-item', () => {
+    const div = wrapper.find('div');
+    expect(div.hasClass('grid-item')).toBeTruthy();
   });
 });
